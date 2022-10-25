34cc7abfbcbe7b01c61d11508c4a3c5fd4c0771e
Gunnar <gunnarmani18@gmail.com>
2022-10-25 14:24:42 +0200

    car

--- 7_Assignment/Classes/car.cs
+++ 7_Assignment/Classes/car.cs
@@ -1,11 +1,12 @@
-class Car {
+class Car
+{
     #region Fields
-    
-    public string Brand = ""; 
+
+    public string Brand = "";
     protected int CurrentGear = 0;
     private float _speed = 0f;
     internal int _revolutions = 0;
-    
+
     public List<Door> Doors;
     public List<Tire> Tires;
 
@@ -13,7 +14,8 @@ class Car {
 
     #region Constructors
 
-    public Car(string brandParamater){
+    public Car(string brandParamater)
+    {
         this.Brand = brandParamater;
         this.Tires = new List<Tire>();
         this.Doors = new List<Door>();
@@ -21,14 +23,22 @@ class Car {
         this.Doors.Add(door1);
         this.Doors.Add(new Door());
 
-        for(int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++)
+        {
             this.Tires.Add(new Tire(10));
         }
         this.GetIn(this.Doors[0]);
         Console.WriteLine("Car constructed / Instantiated");
     }
 
-    public Car(string brandParamater, int amountOfDoors, int amountOfTires, int tireSize, string rimMaterial){
+    public Car(
+        string brandParamater,
+        int amountOfDoors,
+        int amountOfTires,
+        int tireSize,
+        string rimMaterial
+    )
+    {
         //Try to fill out the overloaded constructor
         Console.WriteLine("Car constructed / Instantiated");
     }
@@ -36,21 +46,30 @@ class Car {
     #endregion
 
     #region Methods
-    
-    void Break(){
+
+    void Brake()
+    {
         _speed = 0f;
     }
 
-    public void Accelerate(float forceParameter){
+    public void Accelerate(float forceParameter)
+    {
         _speed = _speed + forceParameter;
     }
-    
-    public int ChangeGear(int amount){
+
+    public void Turbo(float forceParameter)
+    {
+        _speed = _speed * forceParameter;
+    }
+
+    public int ChangeGear(int amount)
+    {
         CurrentGear += amount;
         return CurrentGear;
     }
 
-    private void GetIn(Door doorParameter){
+    private void GetIn(Door doorParameter)
+    {
         doorParameter.Open();
         Console.WriteLine("Got inside " + Brand);
         doorParameter.Close();
